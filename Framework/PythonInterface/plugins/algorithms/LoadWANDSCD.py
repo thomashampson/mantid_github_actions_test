Fix `LoadWANDSCD` normalization scale for "Counts" and update tests w… (#41647)
Signed-off-by: Jose Borreguero <borreguero@gmail.com>

Framework/PythonInterface/plugins/algorithms/LoadWANDSCD.py
@@ -484,8 +484,8 @@ class LoadWANDSCD(PythonAlgorithm):
         DeleteWorkspace(norm_replicated)
         # find the scale
         if normalize_by == "counts":
-            scale = norm.getSignalArray().mean()
-            self.getLogger().information("scale counts = {}".format(int(scale)))
+            scale = 1.0 / norm.getSignalArray().mean()
+            self.getLogger().information("scale counts = {}".format(scale))
         elif normalize_by == "monitor":
             scale = np.array(data.getExperimentInfo(0).run().getProperty("monitor_count").value)
             scale /= norm.getExperimentInfo(0).run().getProperty("monitor_count").value[0]